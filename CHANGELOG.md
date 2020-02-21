95182bf947b66ddcf091a8b307f0514538393d42
Jakub Jankiewicz <jakub.jankiewicz@contractors.roche.com>
2020-02-21 10:27:45 +0100

    add load function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.4.0
+### Features
+* load function
+
 ## 0.3.1
 ### Bugfix
 * refactor static services
--- R/helpers.R
+++ R/helpers.R
@@ -14,3 +14,29 @@ invoke <- function(fn, ...) {
 debug <- function(...) {
   print(paste(list(...), collapse = " "))
 }
+
+#' Function load all R file components from directory
+#' @param path - path to directory
+#' @param pattern - pattern used to selected the files (default all R files)
+#' @param ignore - string or vector of strings with fillanme files that should be ignored
+#' @param recursive - if set to TRUE (default) loading of files will be recursive
+#' @export
+load <- function(path, pattern = "*.R$", ignore = NULL, recursive = TRUE) {
+  components <- list.files(
+    path = path,
+    pattern = pattern,
+    recursive = recursive,
+    full.names = TRUE
+  )
+  if (is.null(ignore)) {
+    for (path in components) {
+      source(path, local = FALSE)
+    }
+  } else {
+    for (path in components) {
+      if (all(path != ignore)) {
+        source(path, local = FALSE)
+      }
+    }
+  }
+}
